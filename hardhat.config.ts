9d2b2306dad3a8eac449167e3a12ce12b9c6f23b
Tahlil <bsse0803@iit.du.ac.bd>
2023-01-31 23:41:19 +0600

    Add gas report functionality for create subscription function

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'hardhat-gas-reporter';
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+
+const coinmarketAPIKey = process.env.COINMARKETCAP_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21,
+    enabled: true,
+    coinmarketcap: coinmarketAPIKey
+  }
 };
 
 export default config;
--- test/Subscription.ts
+++ test/Subscription.ts
@@ -14,31 +14,41 @@ describe("Subscription", function () {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Subscription = await ethers.getContractFactory("Subscription");
-    const subscription = await Subscription.deploy();
+    const subscription = await Subscription.deploy(owner.address, owner.address);
 
     return { subscription, owner, otherAccount };
   }
 
 describe("Test Suite", function () {
-    it("Should deploy", async function () {
-      const { subscription, } = await loadFixture(deployOneYearSubscriptionFixture);
+    it("Run subscription once", async function () {
+      const { subscription, otherAccount } = await loadFixture(deployOneYearSubscriptionFixture);
+      let tx = await subscription.createSubscription(otherAccount.address, 11, 11, 1);
+      await tx.wait();
     });
+    it("Run subscription once", async function () {
+      const { subscription, otherAccount } = await loadFixture(deployOneYearSubscriptionFixture);
+      let tx = await subscription.createSubscription(otherAccount.address, 11, 11, 1);
+      await tx.wait();
+      tx = await subscription.createSubscription(otherAccount.address, 11, 11, 1);
+      await tx.wait();
+    });
+
 });
 
 
-describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { subscription, unsubscriptionTime, subscriptionedAmount } = await loadFixture(
-          deployOneYearSubscriptionFixture
-        );
+// describe("Events", function () {
+//       it("Should emit an event on withdrawals", async function () {
+//         const { subscription } = await loadFixture(
+//           deployOneYearSubscriptionFixture
+//         );
 
-        await time.increaseTo(unsubscriptionTime);
+//         await time.increaseTo(unsubscriptionTime);
 
-        await expect(subscription.withdraw())
-          .to.emit(subscription, "CustomEvent")
-          .withArgs(subscriptionedAmount, anyValue); // We accept any value as `when` arg
-      });
-});
+//         await expect(subscription.withdraw())
+//           .to.emit(subscription, "CustomEvent")
+//           .withArgs(subscriptionedAmount, anyValue); // We accept any value as `when` arg
+//       });
+// });
 
 
 });
